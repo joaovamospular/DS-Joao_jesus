4aea31a189299d1485852ec855253cd89a19a0d2
Joao Vitor Vale <Joodraw5@gmail.com>
2025-04-14 07:52:08 -0300

    Exemplo de matriz, visto em aula

---
+++ ExerciciosMatrizeseJava/Matriz.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package matriz;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Matriz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    cars [0] = "Opel";
+    System.out.println(cars[0]);
+  }
+
+
+ }
