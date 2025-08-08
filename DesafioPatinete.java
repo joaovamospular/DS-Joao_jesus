9c7a2e1abdf3c4405cb0afaca795875ea7358c20
Joao Vitor <Joodraw5@gmail.com>
2025-08-08 19:43:32 -0300

    Desafios passados na aula passada

---
+++ DesafioPatinete.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+// Importo java util Scanner para poder receber os dados e o arrays para organizar os numeros
+package desafios;
+import java.util.Arrays;
+import java.util.Scanner;
+/**
+ *
+ * @author joaod
+ */
+public class DesafioPatinete {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Uso o scanner para receber os numeros
+        Scanner sc = new Scanner(System.in);
+       //enquanto sc
+       
+        for (int l = 0; l< 5; l++){
+            int[] nums = new int[8];
+            
+            // Lendo 8 inteiros (2 linhas de 4 valores cada)
+            for (int i = 0; i < 8; i++) {
+                if (sc.hasNextInt()) {
+                    nums[i] = sc.nextInt();
+                } else {
+                    return; // fim da entrada
+                }
+            }
+            
+            Arrays.sort(nums);
+            
+            int soma = 0;
+            for (int n : nums) soma += n;
+            int media = soma / 8; // divisão inteira arredonda para baixo
+            
+            // Imprime array ordenado
+            for (int i = 0; i < nums.length; i++) {
+                System.out.print(nums[i]);
+                if (i < nums.length - 1) System.out.print(" ");
+            }
+            System.out.println(" - " + media);
+        }
+        
+        sc.close();
+    }
+    
+}
---
+++ DesafioVacina.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package desafios;
+import java.util.Scanner;
+/**
+ *
+ * @author joaod
+ */
+
+public class DesafioVacina {
+    
+    public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextInt()) {
+            int N = sc.nextInt();
+            int[] genes = new int[N];
+
+            for (int i = 0; i < N; i++) {
+                genes[i] = sc.nextInt();
+            }
+
+            int alvo = sc.nextInt();
+            int contagem = 0;
+
+            for (int gene : genes) {
+                if (gene == alvo) {
+                    contagem++;
+                }
+            }
+
+            if (contagem > 1) {
+                System.out.println("Achei o danado");
+            } else if (contagem == 1) {
+                System.out.println("Ainda não é suficiente");
+            } else {
+                System.out.println("Sumiu!");
+            }
+        }
+
+        sc.close();
+}
+}
